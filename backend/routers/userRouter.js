926dbf6d660416d6f70fae4cfcca4a0e6847e90c
HichamBen <hbl73801@gmail.com>
2023-11-27 13:34:05 +0100

    update dependencies

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,82 +1,103 @@
 const express = require("express");
 const data = require("../data");
 const expressAsyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
+const { generateHash, isValidPass } = require("../config/genValidPass");
 const User = require("../models/userModel");
 const { generateToken, isAuth } = require("../utils");
 
 const userRouter = express.Router();
 
-userRouter.get("/seed", expressAsyncHandler(async (req, res) => {
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
     // await User.deleteMany({});
-    const createdUsers = await User.insertMany(data.users);
-    res.send({ createdUsers });
-}));
+    // const createdUsers = await User.insertMany(data.users);
+    // res.send({ createdUsers });
+     res.send("They created.");
 
-userRouter.post("/signin", expressAsyncHandler(async (req, res) => {
+  })
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
 
     if (user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-            res.send({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user),
-            });
-            return;
-        }
+      const hashSalt = user.password.split("++");
+      if (isValidPass(req.body.password, hashSalt[0], hashSalt[1])) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
     }
 
-    res.status(401).send({ message: "Invalid email or password" })
-}));
+    res.status(401).send({ message: "Invalid email or password" });
+  })
+);
 
-userRouter.post("/register", expressAsyncHandler(async (req, res) => {
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+    const { salt, hash } = generateHash(req.body.password);
+
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10),
+      name: req.body.name,
+      email: req.body.email,
+      password: `${hash}++${salt}`,
     });
     const createdUser = await user.save();
     res.send({
-        _id: createdUser._id,
-        name: createdUser.name,
-        email: createdUser.email,
-        isAdmin: createdUser.isAdmin,
-        token: generateToken(createdUser),
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
     });
-}));
+  })
+);
 
-userRouter.get("/:id", expressAsyncHandler(async (req, res) => {
+userRouter.get(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (user) {
-        res.send(user);
+      res.send(user);
     } else {
-        res.status(404).send({ message: "User Not Found" })
+      res.status(404).send({ message: "User Not Found" });
     }
-}));
+  })
+);
 
-userRouter.put("/profile", isAuth ,expressAsyncHandler(async (req, res) => {
+userRouter.put(
+  "/profile",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
-        user.name = req.body.name || user.name;
-        user.email = req.body.email || user.email;
-        if(req.body.password) {
-            user.password = bcrypt.hashSync(req.body.password, 10);
-        }
-        const updateUser = await user.save();
-        res.send({
-            _id: updateUser._id,
-            name: updateUser.name,
-            email: updateUser.email,
-            isAdmin: updateUser.isAdmin,
-            token: generateToken(updateUser),
-        });
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        const { salt, hash } = generateHash(req.body.password);
+        user.password = `${hash}++${salt}`;
+      }
+      const updateUser = await user.save();
+      res.send({
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+        isAdmin: updateUser.isAdmin,
+        token: generateToken(updateUser),
+      });
     } else {
-        res.status(404).send({ message: "User Not Found" })
+      res.status(404).send({ message: "User Not Found" });
     }
-}));
+  })
+);
 
 module.exports = userRouter;
-
--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI);
-
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
@@ -28,9 +26,19 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`The server is running on port ${port}`);
-});
+const PORT = process.env.PORT || 5000;
 
-module.exports = app
+// connect to Mongodb then open the server
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`The server run on ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log("mongodb error:", err.message);
+  });
+
+module.exports = app;
